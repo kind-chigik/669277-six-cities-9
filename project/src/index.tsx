f0467dae005c307028f41e11fba1609117a0674d
Инна Русина <rus.work89@yandex.ru>
2022-02-15 00:29:32 +0300

    Добавлен пропс для компонента App

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  OFFERS_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersCount = {Settings.OFFERS_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
+
