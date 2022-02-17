1968909a66b697082e246cc1e77494f7469ff914
Инна Русина <rus.work89@yandex.ru>
2022-02-17 20:53:55 +0300

    Настроена маршрутизация, скорректирована разметка

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,32 @@
 import Main from '../main/main';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Offer from '../offer/offer';
+import NotFound from '../not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 
 type OfferProps = {
   offersCount: number;
 }
 
 function App(props: OfferProps): JSX.Element {
-  return <Main offersCount = {props.offersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Main offersCount = {props.offersCount} />}></Route>
+        <Route path={AppRoute.Login} element={<Login />}></Route>
+        <Route path={AppRoute.Favotites} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}><Favorites /></PrivateRoute>
+        }
+        >
+        </Route>
+        <Route path={AppRoute.Offer} element={<Offer />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
